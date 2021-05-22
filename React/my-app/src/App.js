8e04130aa0482936b36d15efa936b01dc74b9076
Himalaya293 <73583283+Himalaya293@users.noreply.github.com>
2021-05-22 14:18:03 +0530

    first commit

---
+++ React/my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        div className = "Hello World" >
+        <
+        h1 > Components and Templates < /h1> <
+        /div> <
+        /div>
+    );
+}
+
+export default App;
